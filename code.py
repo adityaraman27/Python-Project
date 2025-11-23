c589f0f3993ebdd2e21b65f19e0c99d90770ebd6
adityaraman27 <2adityaraman@gmail.com>
2025-11-23 12:34:41 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 import random
-word_list = ["greninja","goodra", "talonflame", "noibat","pikachu", "hawlucha", "balloon", "banana"]
+word_list = [ "accuracy", "adaptive", "advisor", "alleged", "analyze", "apparent", "assumed", "attitude", "awareness", "behavior", "benefit", "blizzard", "capable", "career", "cautious", "champion", "climate", "collapse", "commerce", "community", "confirm", "conquer", "crisis", "current", "declared", "defense", "deliver", "demand", "destroy", "developer", "digital", "discover", "drastic", "elegant", "emerging", "endeavor", "estimate", "ethical", "except", "executive", "exhibit", "expected", "failure", "fantastic", "feasible", "feature", "formula", "forward", "frequent", "frustrate", "generate", "globally", "graduate", "habitat", "honesty", "identity", "illusion", "impact", "improve", "include", "industry", "inspect", "instinct", "integral", "interact", "involve", "isolate", "judgment", "justify", "kindness", "legendary", "located", "logical", "magnify", "maximum", "merchant", "minister", "momentum", "movement", "multiple", "necessary", "observer", "official", "operate", "origin", "outcome", "overview", "partner", "pattern", "perform", "philosophy", "popular", "predict", "prepare", "previous", "priority", "professor", "proposal", "protect", "publish", "balloon", "banana"]
 word = random.choices(word_list)
 fword = word[0]
 print(word)

40ab41c4014bf8d0fcea9dc2316a63fb85e3722f
adityaraman27 <2adityaraman@gmail.com>
2025-11-23 12:00:36 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,108 @@
+bimport random
+word_list = ["greninja","goodra", "talonflame", "noibat","pikachu", "hawlucha", "balloon", "banana"]
+word = random.choices(word_list)
+fword = word[0]
+print(word)
+word_len = len(word[0])
+wordl=list(fword)
+guess = [ "_" for i in range(len(fword))]
+print(guess)
+copyl = wordl.copy()
+total_life = 6
+stages = [  
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+while total_life > 0:
+    inp = input("Enter letter :" )
+    if inp in wordl :
+        c=wordl.count(inp)
+        for i in range(c):
+            l = copyl.index(inp)
+            guess[l] = inp
+            copyl[l] = "*"
+        print(guess) 
+    else :
+         total_life -=1
+    if total_life == 6 :
+        print(stages[0])
+    if total_life == 5 :
+        print(stages[1])    
+    if total_life == 4 :
+        print(stages[2])
+    if total_life == 3 :
+        print(stages[3])
+    if total_life == 2 :
+        print(stages[4])
+    if total_life == 1 :
+        print(stages[5])      
+    if total_life == 0 :
+        print(stages[6])
+        print("YOU LOST !!")
